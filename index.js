a214a13b93fb664dda46b3cfed04e9d09eaf3c33
developermaxwellkaranja <developermaxwellkaranja@gmail.com>
2023-03-03 22:55:15 +0300

    third commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var sidebar = document.getElementById("sidebar");
+   function showMenu(){
+    sidebar.style.right = "0";
+   }
+   
+   function hideMenu(){
+    sidebar.style.right = "40rem";
+   }
+   
+
+
+var typed = new Typed(".input", {
+strings:["the MaxShan duo💑",  "Great friends👩🏽‍🤝‍🧑🏼", "together as one🤞","the best couple ever🥰", "meant for greatness and we are going far by the grace of our creator✨"],
+typeSpeed: 150,
+backSpeed: 40,
+loop: true,
+})
+
